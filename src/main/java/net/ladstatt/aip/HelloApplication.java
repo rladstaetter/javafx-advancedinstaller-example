2a4015e558f768174d23e550533845687f6beac5
LADSTÄTTER Robert <robert.ladstaetter@hexagon.com>
2022-10-08 18:16:22 +0200

    Initial commit

---
+++ src/main/java/net/ladstatt/aip/HelloApplication.java
@@ -0,0 +1,9 @@
+package net.ladstatt.aip;
+
+public class HelloApplication {
+    public static void main(String[] args) {
+        javafx.application.Application.launch(HelloApp.class, args);
+    }
+
+}
+
